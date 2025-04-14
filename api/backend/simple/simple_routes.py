64be59f1626657d6abe385d76cb9e4a212aee5d8
Sophia10101 <sskater2914@gmail.com>
2025-04-13 22:09:17 -0400

    Created routes for persona 1, two user strories

--- api/backend/simple/simple_routes.py
+++ api/backend/simple/simple_routes.py
@@ -57,10 +57,24 @@ def mesage():
 # ------------------------------------------------------------
 @simple_routes.route('/fda/personalized_suggestions', methods = ['GET'])
 def get_personalized_suggestions():
-    return 
+    cursor = db.get_db().cursor()
+    the_query = '''
+    SELECT ps.SuggestionID, ps.GroupSize, FDA.pricing
+    FROM personalizedSuggestions ps
+    JOIN foodDecoActivities FDA on ps.SuggestionID = FDA.SuggestionID
+    WHERE ps.GroupSize >= %s
+    AND FDA.Pricing <= %s
+    '''
+    cursor.execute(the_query)
+    theData = cursor.fetchall()
+    the_response = make_response(theData)
+    the_response.status_code = 200
+    the_response.mimetype='application/json'
+    return the_response
+
 # ------------------------------------------------------------
 @simple_routes.route('/complaints', methods = ['POST'])
-def get_personalized_suggestions():
+def create_complaint():
     return 
 # ------------------------------------------------------------
 @simple_routes.route('/fda/personalized_suggestions/ presets', methods = ['PUT'])
@@ -69,4 +83,19 @@ def save_preset():
 # ------------------------------------------------------------
 @simple_routes.route('/users/subscription', methods = ['PUT'])
 def update_subscription():
-    return 
+    current_app.logger.info('PUT /users/subscription route')
+    user_info = request.json
+    user_id = user_info['id']
+    sub = user_info['subscription']
+
+    query = '''
+            UPDATE subscription s 
+            JOIN accounts a ON s.AccountID = a.AccountID 
+            SET s.Pro = TRUE, s.Free = FALSE
+            WHERE a.AccountID = %s AND a.UserID = %s
+            '''
+    data = (sub, user_id)
+    cursor = db.get_db().cursor()
+    r = cursor.execute(query, data)
+    db.get_db().commit()
+    return 'subscription updated!'

fe3622f9598784701ac526f5f9a0739ebd06821a
Diaz0705 <Diaz.bra@northeastern.edu>
2025-04-16 03:03:18 +0000

    Persona 3 streamlit changes Part 1

--- app/src/pages/02_Data_Analyst_User_Home.py
+++ app/src/pages/02_Data_Analyst_User_Home.py
@@ -2,6 +2,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 import streamlit as st
+import requests
 from modules.nav import SideBarLinks
 
 st.set_page_config(layout = 'wide')
@@ -9,9 +10,16 @@ st.set_page_config(layout = 'wide')
 # Show appropriate sidebar links for the role of the currently logged in user
 SideBarLinks()
 
+# Session check
+if 'first_name' not in st.session_state:
+    st.error("You're not logged in. Please return to the login page.")
+    st.stop()
+
 st.title(f"Welcome Data Analyst {st.session_state['first_name']}.")
-st.write('')
-st.write('')
+st.write("Here are your available insights at a glance.")
+st.write("")
+
+
 st.write('### What would you like to do today?')
 
 if st.button('View Reports Page', 
